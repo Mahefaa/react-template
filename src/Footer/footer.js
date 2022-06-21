9323d78a90599c5adf5c970af4f91c531a77bbdd
Mahefa Ny Anjara <hei.mahefa@gmail.com>
2022-06-21 17:28:55 +0300

    original code without css

---
+++ src/Footer/footer.js
@@ -0,0 +1,12 @@
+export default function Footer(props){
+return(
+    <footer>
+        <div>
+            <p>@Copyright Toky Transport Powered by HEI Tech</p>
+        </div>
+        <div>
+            <a href="#">Terms of use</a>
+        </div>
+    </footer>
+);
+}
---
+++ src/Header/header.js
@@ -0,0 +1,28 @@
+export default function Header(props){
+return(
+    <header>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">Accueil</a>
+                </li>
+                <li>
+                    <a href="#">Voyage</a>
+                </li>
+                <li>
+                    <a href="#">Services</a>
+                </li>
+                <li>
+                    <a href="#">Informations</a>
+                </li>
+                <li>
+                    <a href="#">Aide</a>
+                </li>
+                <li>
+                    <a href="#">Langue</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+)
+}
