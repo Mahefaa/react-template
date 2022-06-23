2bf4821d7ddc4848993cf05a948e70a057d26d02
Mahefa Ny Anjara <hei.mahefa@gmail.com>
2022-06-23 17:28:18 +0300

    feat : better navbar

--- src/Header/header.js
+++ src/Header/header.js
@@ -6,19 +6,21 @@ return(
         <nav className="navbar navbar-expand-lg navbar-light bg-info row">
             <div className={"col-4"}>
             </div>
-            <a className="navbar-brand" href="#">Navbar</a>
+            <a className="navbar-brand text-danger" href="#">Toky Transport</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
-                    aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation" onClick={()=>{
+
+            }}>
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div className="navbar-nav">
                     <a className="nav-link active" href="#">Home <span className="sr-only">(current)</span></a>
-                    <a className="nav-link" href="#">Voyages</a>
+                    <a className="nav-link" href="#">Trip</a>
                     <a className="nav-link" href="#">Services</a>
                     <a className="nav-link">Informations</a>
-                    <a className="nav-link">Aide</a>
-                    <a className="nav-link">Langues</a>
+                    <a className="nav-link">Help</a>
+                    <a className="nav-link">Language</a>
                 </div>
             </div>
         </nav>
