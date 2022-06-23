05c5b9eac301475ab3b5e5aa8d5256df4b2f8c7a
Mahefa Ny Anjara <hei.mahefa@gmail.com>
2022-06-23 17:28:43 +0300

    feat : enhanced footer

--- src/Footer/footer.js
+++ src/Footer/footer.js
@@ -1,12 +1,18 @@
 import './footer.css';
-import '../bootstrap-4.6.1-dist/css/bootstrap.css'
+import '../bootstrap-4.6.1-dist/css/bootstrap-grid.css'
 export default function Footer(props){
 return(
-    <footer className={"jumbotron d-flex"}>
-        <div className={""}>
+    <footer className={"jumbotron d-flex mr-2 ml-2 mb-0 bg-info"}>
+        <div className={"col"}>
             <p>@Copyright Toky Transport Powered by HEI Tech</p>
         </div>
-        <div className={""}>
+        <div className={"col-7"}>
+            <p>
+                All issues should be sent to :
+                <a href={" mailto: tokyTransport.example.valid "}><strong>{" "}yourmail@example.valid </strong></a>
+            </p>
+        </div>
+        <div className={"col"}>
             <a href="#">Terms of use</a>
         </div>
     </footer>
